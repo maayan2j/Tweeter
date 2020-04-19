ad2fbe172aecdb1924d82f19958a65323460c76d
maayan2j <maayan2j@gmail.com>
2020-04-19 22:48:44 +0300

    Tweeter

---
+++ logic.js
@@ -0,0 +1,75 @@
+/*
+Author: Anas Nasrallah. 
+Purpose: Tweeter Project.
+Date: 08.04.20
+*/
+
+/* This file represents the Logic Module, a module which is 
+responsible for maintaining the data of the program (the posts
+and the comments).
+It returns several function getPosts, addPost, removePost,
+addComment, removeComment */
+
+const Tweeter = function(){
+
+    let posts =  [];
+    let postIdCounter = 0;
+    let commentIdCounter = 0;
+
+    const getPosts = function(){
+        return posts
+    }
+
+    const addPost = function(text){
+        newPost = {
+            id: "p" + (postIdCounter + 1),
+            text: text,
+            comments: []
+        }
+        posts.push(newPost);
+        postIdCounter++;
+    }
+
+    const removePost = function(postID){
+        for(let i = 0; i < posts.length; i++){
+            if(posts[i].id === postID){
+                posts.splice(i, 1);
+            }
+        }
+    }
+
+    const addComment = function(text, postID){
+        newComment = {
+            id: "c" + (commentIdCounter + 1),
+            text: text
+        }
+        for(let post of posts){
+            if (post.id === postID){
+                post.comments.push(newComment);
+                commentIdCounter++;
+            }
+        }
+    }
+
+    const removeComment = function(postID, commentID){
+        for(let post of posts){
+            if (post.id === postID){
+                for(let i = 0; i < post.comments.length; i++){
+                    if(post.comments[i].id === commentID){
+                        post.comments.splice(i, 1);  
+                    }
+                }
+            }
+        }
+    }
+
+    return{
+        getPosts,
+        addPost,
+        removePost,
+        addComment,
+        removeComment
+    }
+}
+
+
---
+++ render.js
@@ -0,0 +1,54 @@
+/*
+Author: Anas Nasrallah. 
+Purpose: Tweeter Project.
+Date: 17.04.20
+*/
+
+/* This file represents the Renderer Module, a module which is 
+responsible for rendering the whole program. 
+It returns one function renderPosts() */
+
+const Renderer = function(){
+
+    // A function that creates and returns a div for a single post
+    const createPost = function(post){
+        $postdiv = $(`<div>${post['text']}</div>`);
+        $postdiv.attr('class', 'post');
+        $postdiv.attr('data-id', post['id']);
+        $postdiv.attr('data-text', post['text']);
+        
+        return $postdiv;
+    }
+
+    // A function that creates and returns a div for a single post
+    const createComment = function(comment){
+        commentDel = '<span class="delete-comment">X</span>'
+        $commentdiv = $(`<div>${commentDel} ${comment['text']}</div>`);
+        $commentdiv.attr('class', 'comments');
+        $commentdiv.attr('data-id', comment['id']);
+        $commentdiv.attr('data-text', comment['text']);
+        
+        return $commentdiv;
+    }
+
+    const renderPosts = function(posts){
+        $('#posts').empty();
+        $posts = $('#posts');
+        for(let post of posts){
+            $postdiv = createPost(post);
+            for(let comment of post['comments']){
+                $commentdiv = createComment(comment);
+                $postdiv.append($commentdiv);
+            }
+            commentInput = '<br><input class = "commentText" placeholder="Got something to say">'
+            commentBtn = '<button class="addComment">Comment</button><br>'
+            $postdiv.append($(commentInput + commentBtn))
+            $postdiv.append($('<div class = "delete"> Delete Post </div>'))
+            $posts.append($postdiv)
+        }
+    }
+    
+    return {
+        renderPosts
+    }
+}
